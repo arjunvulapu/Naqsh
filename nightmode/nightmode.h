5765da2c11a4a7d5cf41f5b35aa938cf7f298333
Amit Kulkarni <pythoncpp@gmail.com>
2017-01-03 11:01:36 +0530

    Initial Commit

---
+++ nightmode/nightmode.h
@@ -0,0 +1,19 @@
+//
+//  nightmode.h
+//  nightmode
+//
+//  Created by Amit Kulkarni on 03/01/17.
+//  Copyright © 2017 Amit Kulkarni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for nightmode.
+FOUNDATION_EXPORT double nightmodeVersionNumber;
+
+//! Project version string for nightmode.
+FOUNDATION_EXPORT const unsigned char nightmodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <nightmode/PublicHeader.h>
+
+
